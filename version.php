201ea5ddb138a3713891b447bd6be73b9cb49aeb
Alexander Van der Bellen <alexandervanderbellen@catalyst-au.net>
2025-04-30 20:42:53 +0800

    Add condition for user interests

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'tool_dynamic_cohorts';
-$plugin->release = 2024112100;
-$plugin->version = 2024112101;
+$plugin->release = 2025050100;
+$plugin->version = 2025050100;
 $plugin->requires = 2022112800;
 $plugin->supported = [404, 405];
 $plugin->maturity = MATURITY_STABLE;
